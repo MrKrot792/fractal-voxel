df00c35f65d3b3624982eceddaf07825d54ea61c
Mr Krot <vkorotanec@gmail.com>
2026-06-20 22:03:03 +0200

    New entity interface, new package: bitflags

--- src/entities/entity.rs
+++ src/entities/entity.rs
@@ -1,40 +1,65 @@
 use std::collections::{VecDeque, HashMap};
-use std::rc::Rc;
 use std::cell::RefCell;
 use winit::event::{DeviceEvent, ElementState, KeyEvent, MouseButton, WindowEvent};
 use crate::renderer::instance;
 use crate::fps;
+use std::any::Any;
 
+/// Different kinds of events.
+/// They're self explinatory.
 #[derive(Debug)]
 pub enum Event {
   Resized(winit::dpi::PhysicalSize<u32>),
   Key(KeyEvent),
+  /// (f64, f64) is mouse's delta.
   MouseMotion((f64, f64)),
   Mouse(ElementState, MouseButton),
 }
 
-// good enough for now i guess
-pub trait Entity {
-  fn update(&mut self, entity_index: &usize, render_context: &mut instance::RenderContext, fps: &fps::Fps) -> anyhow::Result<()> {
-    _ = entity_index;
-    _ = render_context;
-    _ = fps;
-    Ok(())
+bitflags::bitflags! {
+  /// Must be returned in the init function.
+  /// This defines what callbacks the entity will need, for example, a
+  /// simple fps daemon doesn't care about events. So it can just
+  /// return [`UPDATE`].
+  pub struct RequestedCallbacks: u32 {
+    /// Called on update, usually 60 times per second
+    const UPDATE = 1 << 0;
+    /// Called on any event
+    /// This should be only used for things like raw mouse input,
+    /// resizing, etc. For keyboard inputs, [`KeyEvent`] exists.
+    const EVENT =  1 << 1;
+    /// Called whenever the entity's ID changes. Usually, it never
+    /// happens, so this is reserved for future use.
+    const SET_ID = 1 << 2;
   }
-  fn event(&mut self, entity_index: &usize,  render_context: &mut instance::RenderContext, event: &Event) -> anyhow::Result<()> {
-    _ = entity_index;
-    _ = render_context;
-    _ = event;
-    Ok(())
-  }
-  fn set_id(&mut self, new_id: usize);
 }
 
+pub type EntityId = usize;
+
+/// A trait for general kind of entity, for something that updates in
+/// the game. For example, some kind of manager.
+/// The Entity must save it's ID when [`init()`] is called.
+pub trait Entity: Any {
+  /// The only needed function.
+  fn init(&mut self, id: EntityId) -> RequestedCallbacks;
+  fn update(&mut self, _render_context: &mut instance::RenderContext, _fps: &fps::Fps) -> anyhow::Result<()> { Ok(()) }
+  fn event(&mut self, _render_context: &mut instance::RenderContext, _event: &Event) -> anyhow::Result<()> { Ok(()) }
+  fn set_id(&mut self, _new_id: EntityId) -> anyhow::Result<()> { Ok(()) }
+}
+
+struct EntityWithCallbacks {
+  entity: RefCell<Box<dyn Entity>>,
+  callbacks: RequestedCallbacks,
+}
+
+// TODO: Maybe add multiple Vec's for storing referemces to
+// entities with different callbacks, so it's faster to call
+// them.
 #[derive(Default)]
 pub struct EntityManager {
   event_queue: VecDeque<Event>,
-  entities: HashMap<usize, Rc<RefCell<dyn Entity>>>,
-  entity_last_id: usize,
+  entities: HashMap<EntityId, EntityWithCallbacks>,
+  entity_last_id: EntityId,
 }
 
 impl EntityManager {
@@ -42,20 +67,26 @@ impl EntityManager {
     Self::default()
   }
   
-  pub fn entity_create<T: Entity + 'static>(&mut self, entity: Rc<RefCell<T>>) -> usize {
-    entity.borrow_mut().set_id(self.entity_last_id);
-    self.entities.insert(self.entity_last_id, entity);
+  pub fn entity_create<T: Entity + 'static>(&mut self, mut entity: T) -> EntityId {
+    let actual_entity = EntityWithCallbacks {
+      callbacks: entity.init(self.entity_last_id),
+      entity: RefCell::new(Box::new(entity)),
+    };
+     
+    self.entities.insert(self.entity_last_id, actual_entity);
     self.entity_last_id += 1;
     self.entity_last_id - 1
   }
 
-  pub fn entity_destroy(&mut self, entity: usize) -> Option<Rc<RefCell<dyn Entity>>> {
-    self.entities.remove(&entity)
+  pub fn entity_destroy(&mut self, entity: EntityId) {
+    self.entities.remove(&entity).unwrap();
   }
 
   pub fn update(&mut self, render_context: &mut instance::RenderContext, fps: &fps::Fps) -> anyhow::Result<()> {
-    for (k, v) in &self.entities {
-      v.borrow_mut().update(k, render_context, fps)?
+    for v in self.entities.values_mut() {
+      if !v.callbacks.contains(RequestedCallbacks::UPDATE) { continue; }
+      
+      v.entity.borrow_mut().update(render_context, fps)?
     }
 
     Ok(())
@@ -81,13 +112,14 @@ impl EntityManager {
   }
 
   pub fn dispatch_events(&mut self, render_context: &mut instance::RenderContext) -> anyhow::Result<()> {
-    for event in self.event_queue.iter() {
-      for (k, v) in self.entities.iter_mut() {
-	v.borrow_mut().event(k, render_context, event)?;
+    for v in self.entities.values_mut() {
+      if !v.callbacks.contains(RequestedCallbacks::EVENT) { continue; }
+      for event in self.event_queue.iter() {
+	v.entity.borrow_mut().event(render_context, event)?;
       }
     }
 
     self.event_queue.clear();
     Ok(())
-  } 
+  }
 }
